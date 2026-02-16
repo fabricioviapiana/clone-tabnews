d2d15ad4cb43da2c2fe0b809141bdc1083438d38
Fabricio Viapiana <fabriciofviapiana@gmail.com>
2026-02-16 10:57:37 -0300

    feat: require create:user to access POST /users

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -125,4 +125,34 @@ describe("POST /api/v1/users", () => {
       });
     });
   });
+
+  describe("Default user", () => {
+    test("Already logged user with unique and valid data", async () => {
+      const createdUser = await orchestrator.createUser();
+      await orchestrator.activateUser(createdUser.id);
+      const userSession = await orchestrator.createSession(createdUser.id);
+
+      const response = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Cookie: `session_id=${userSession.token}`,
+        },
+        body: JSON.stringify({
+          username: "fabricioffv",
+          email: "fabriciofviapiana@gmail.com",
+          password: "senha123",
+        }),
+      });
+      const responseBody = await response.json();
+
+      expect(response.status).toBe(403);
+      expect(responseBody).toEqual({
+        name: "ForbiddenError",
+        message: "Você não possui permissão para executar esta ação",
+        action: `Verifique se o usuário possui a feature create:user`,
+        status_code: 403,
+      });
+    });
+  });
 });
